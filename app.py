6a36975ce1c6d68ff8bcb96bc3e33392353ead1a
Majok-Bol <bolmajok001@gmail.com>
2026-06-10 13:38:39 +0300

    configured sqlalchemy database

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,redirect,request,flash
 from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField,BooleanField,EmailField,PasswordField,SubmitField
 from wtforms.validators import InputRequired,Email,EqualTo,Length
 from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect
+import email_validator
+from flask_login import LoginManager,login_required,login_user,UserMixin,current_user
+from urllib.parse import urljoin,urlparse,parse_qs
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -17,27 +20,98 @@ csrf=CSRFProtect()
 csrf.init_app(app)
 #initialize app with the database
 db=SQLAlchemy(app)
+#initialize app with LoginManager class to manage user logins
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
 #define route
 @app.route("/")
 def home():
     return render_template("home.html",name="Bashbytes")
 
-@app.route("/login")
+@app.route("/login",methods=['POST','GET'])
 def login():
-    return render_template("login.html")
-@app.route("/register")
+    #login form instance
+    form=LoginForm()
+    if form.validate_on_submit():
+        #check if user exists in the database
+        user=User.query.filter_by(username=form.username.data).first()
+        if user and user.password==form.password.data:
+            flash('Login successful')
+            login_user(user)
+            next_page=request.args.get("next")
+            if next_page and is_safe_url(next_page):
+                return redirect(next_page)
+        return redirect(url_for('dashboard'))
+    return render_template("login.html",form=form)
+#define how to secure url parsing
+def is_safe_url(target):
+    host_url=urlparse(request.host_url)
+    print("Host url: ",host_url)
+    redirect_url=urlparse(urljoin(request.host_url,target))
+    return(
+        redirect_url.scheme in ("http","https") and host_url.netloc==redirect_url.netloc
+    )
+
+@app.route("/register",methods=['POST','GET'])
 def register():
+    #register form instance
     form=RegisterForm()
+    if form.validate_on_submit():
+        user=User(username=form.username.data,email=form.email.data,password=form.password.data)
+        #add user to the database
+        db.session.add(user)
+        #save changes to the database
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template("register.html",form=form)
+#dashboard
+@app.route('/dashboard',methods=['POST','GET'])
+@login_required
+def dashboard():
+    
+    return render_template('dashboard.html',username=current_user.username)
 
-
-#form instance
+#load user
+#from stored user id
+'''
+You will need to provide a user_loader callback
+This callback is used to reload the user object from the user ID stored in the session. 
+It should take the str ID of a user, and return the corresponding user object
+'''
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+#using urllib
+base_url='https://fakestoreapi.com'
+target_url="/products"
+combined_url=urljoin(base_url,target_url)
+print("Combined url: ",combined_url)
+example_url=urlparse( "https://www.example.com:8080/products/laptop?id=10&sort=price#reviews")
+print("Parsed query string: ",parse_qs(example_url.query))
+print(example_url)
+#register form class
 class RegisterForm(FlaskForm):
     username=StringField("Username",validators=[InputRequired(),Length(min=4)])
     email=EmailField("Email address",validators=[InputRequired(),Email()])
     password=PasswordField("Password",validators=[InputRequired(),Length(max=255)])
     confirm_password=PasswordField("Confirm password",validators=[InputRequired(),])
     submit=SubmitField("Sign up")
+
+#login form class
+class LoginForm(FlaskForm):
+    username=StringField("Username",validators=[InputRequired(),Length(min=4)])
+    password=PasswordField(validators=[InputRequired(),Length(max=255)])
+    submit=SubmitField("Login")
+#create database 
+class User(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(50),nullable=False,unique=True)
+    email=db.Column(db.String(100),nullable=False,unique=True)
+    password=db.Column(db.String(255),nullable=False,unique=True)
+
 #check if app is run from current module
 if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
